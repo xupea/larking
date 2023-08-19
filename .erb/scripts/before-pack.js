2ef16b30acd72df43f3b8e0e6f5a48badeef1152
peter.xu <peter.xu@xinda.im>
2023-08-19 21:06:57 +0800

    feat: update github action for key and secret

---
+++ .erb/scripts/before-pack.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { writeFileSync, readFileSync } = require('fs');
+
+const { version } = require('../../release/app/package.json');
+
+const rootPath = path.join(__dirname, '../..');
+const versionPath = path.join(rootPath, 'version.json');
+
+function updateVersion(v) {
+  const data = readFileSync(versionPath);
+
+  const parsedData = JSON.parse(data);
+  parsedData.version = v;
+  writeFileSync(versionPath, JSON.stringify(parsedData, null, 2));
+}
+
+exports.default = async function beforePack(context) {
+  updateVersion(version);
+};
