645baf67c1e67cb686a7c7808d9d9177c539d0ce
peter.xu <peter.xu@xinda.im>
2023-09-14 16:11:26 +0800

    fix: not show update button

--- src/renderer/layout/titlebar/right/index.tsx
+++ src/renderer/layout/titlebar/right/index.tsx
@@ -23,7 +23,7 @@ export default function Right() {
             format={(number) => `进行中，已完成${number}%`}
           />
         )}
-        {progress === 'download' && <Update />}
+        {progress === 'downloaded' && <Update />}
       </div>
       {isWindows && <div className={cx('window-controls-container')} />}
     </div>
